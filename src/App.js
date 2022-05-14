80d6e3a9a70e9e69a054dc8022ff794756508dad
lrichass1234 <103443851+lrichass1234@users.noreply.github.com>
2022-05-14 19:44:16 +0800

    ase

--- src/App.js
+++ src/App.js
@@ -163,8 +163,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 3) {
-      newMintAmount = 3;
+    if (newMintAmount > 10) {
+      newMintAmount = 10;
     }
     setMintAmount(newMintAmount);
   };
